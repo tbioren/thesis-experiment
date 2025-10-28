d69d6d14d112ac69f0f48b7dc35dc12243bde988
Thomas Bioren <tbioren22@gmail.com>
2025-10-28 13:58:07 -0400

    new scoring algorithm

--- experiment/tb.v
+++ experiment/tb.v
@@ -4,8 +4,9 @@ module tb;
     reg [3:0] in;
     wire out;
     integer i;
-    integer score = 0;
+    integer total_distance = 0;
     integer total = 0;
+    real score;
 
     // DUT
     cgp_module dut (
@@ -20,6 +21,12 @@ module tb;
         expected = v[0] ^ v[1] ^ v[2] ^ v[3];
     endfunction
 
+    // Compute Hamming distance between two bits
+    function integer hamming_bit;
+        input a, b;
+        hamming_bit = (a !== b) ? 1 : 0;
+    endfunction
+
     initial begin
         // Enable VCD dump
         $dumpfile("tb.vcd");
@@ -29,13 +36,17 @@ module tb;
         for (i = 0; i < 16; i = i + 1) begin
             in = i;
             #5; // wait for signals to propagate
+
+            // add 1 if the bits differ
+            total_distance = total_distance + hamming_bit(out, expected(in));
             total = total + 1;
-            if (out === expected(in))
-                score = score + 1;
+
             #5; // keep each input stable for 10 ns total
         end
 
-        $display("SCORE: %0.10f", 1.0 * score / total);
+        score = 1.0 - (1.0 * total_distance / total);
+        if(score == 0.5) score = 0.0;
+        $display("SCORE: %0.10f", score);
 
         #10; // give VCD time to record last transition
         $finish;
